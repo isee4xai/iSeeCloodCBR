bb48e27f40296bb9bc6d8b23b3e224ab10edd4fc
anjanaw <shyameniw@gmail.com>
2023-10-13 12:45:42 +0100

    test: reuse

--- api/cbrcycle/reuse.py
+++ api/cbrcycle/reuse.py
@@ -14,19 +14,24 @@ def reuse_cases(data):
   # get reuse type
   reuse_type = data.get('reuse_type', None)
   reuse_feature = data.get('reuse_feature', None)
-  print(data)
+
   if reuse_type is None or reuse_feature is None:
     return None
   elif reuse_type.startswith('_'):  # import custom module for use operation
     module_name = 'custom_reuse_scripts.' + reuse_type
     reuse_module = importlib.import_module(module_name)
     # execute and return
-    if reuse_feature is 'transform':
+    if reuse_feature == 'transform':
+      print("reuse_feature", reuse_feature)
       return reuse_module.transform_adapt(data)
-    elif reuse_feature is 'applicability':
+    elif reuse_feature == 'applicability':
+      print("reuse_feature", reuse_feature)
       return reuse_module.applicability(data)
-    elif reuse_feature is 'substitute':
+    elif reuse_feature == 'substitute':
+      print("reuse_feature", reuse_feature)
       return reuse_module.substitute(data)
+    else:
+      return None
   else:  # generic reuse operation
     # logic for any generic reuse operations below
     return None
