3d4aad963d5992722aa2d7a3375ec3284bf9a35c
anjanaw <shyameniw@gmail.com>
2024-02-14 11:49:16 +0000

    update applicability for no ai model upload info

--- api/cbrcycle/custom_reuse_scripts/_isee.py
+++ api/cbrcycle/custom_reuse_scripts/_isee.py
@@ -505,15 +505,6 @@ def explainer_applicability(context, explainer, ontology_props, explain):
                 format_attr(explainer["dataset_type"], 0,
                             "DatasetType", ontology_props) + " data."
 
-    if ANY_URI not in explainer["implementation"] and "implementation" in context and context["implementation"] not in explainer["implementation"]:
-        flag = False
-        if explain:
-            msg = msg+"\n- Implementation Mismatch: This is a " + \
-                format_attr(context["implementation"], 0, "Implementation_Framework", ontology_props) + \
-                " model but " + explainer["name"] + " only supports " + \
-                format_attr(explainer["implementation"], 1,
-                            "Implementation_Framework", ontology_props) + " implementations."
-
     if ANY_URI not in explainer["ai_methods"] and len(set(_i for i in context["ai_method"] for _i in i) & set(explainer["ai_methods"])) == 0:
         flag = False
         if explain:
@@ -530,13 +521,20 @@ def explainer_applicability(context, explainer, ontology_props, explain):
                 format_attr(context["ai_task"], 3, "AITask",
                             ontology_props) + " tasks."
             
-    if ANY_ACCESS_URI != explainer["model_access"] and "model_mode" in context and explainer["model_access"] != context["model_mode"]:
+    if (ANY_URI not in explainer["implementation"]) and ("implementation" not in context or context["implementation"] not in explainer["implementation"]):
+        flag = False
+        if explain:
+            msg = msg+"\n- Implementation Mismatch: " + \
+                explainer["name"] + " only supports " + \
+                    format_attr(explainer["implementation"], 1, "Implementation_Framework", ontology_props) + " implementations."
+
+    if (ANY_ACCESS_URI != explainer["model_access"]) and ("model_mode" not in context or explainer["model_access"] != context["model_mode"]):
         flag = False
         if explain:
             msg = msg+"\n- Model Access Mismatch: " + explainer["name"] + " does not support " + \
                 context["model_mode"]+ " model access."
 
-    if explainer["needs_training_data"] == "true" and "has_training_data" in context  and not context["has_training_data"]:
+    if explainer["needs_training_data"] == "true" and ("has_training_data" not in context  or not context["has_training_data"]):
         flag = False
         if explain:
             msg = msg+"\n- Explainer requires training data."
